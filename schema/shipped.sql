cf4bcd0fb215dcb18be9ed9d7a0f3900e88840f9
Carl Taylor <carl@iodesystems.com>
2026-04-13 17:00:47 -0700

    chore: update shipped.sql

--- schema/shipped.sql
+++ schema/shipped.sql
@@ -2,7 +2,7 @@
 -- PostgreSQL database dump
 --
 
-\restrict kJJfiCoKV0k8W7bkNEu5yP4FdJKcIO3Zytov8oyojb7EKSpydbhb8YtET0XC25E
+\restrict de2RM6aEjGKrwraXYOc7HUQdwgp37IG3hzBTneJ3RaVeQDF1HGAdkkU5ISar5a0
 
 -- Dumped from database version 17.9 (Debian 17.9-1.pgdg13+1)
 -- Dumped by pg_dump version 17.9 (Debian 17.9-1.pgdg13+1)
@@ -2198,5 +2198,5 @@ ALTER TABLE ONLY public.zdx_work_log
 -- PostgreSQL database dump complete
 --
 
-\unrestrict kJJfiCoKV0k8W7bkNEu5yP4FdJKcIO3Zytov8oyojb7EKSpydbhb8YtET0XC25E
+\unrestrict de2RM6aEjGKrwraXYOc7HUQdwgp37IG3hzBTneJ3RaVeQDF1HGAdkkU5ISar5a0
 
